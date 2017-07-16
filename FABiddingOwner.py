eda5c9a44ba9a085888bc349fd439dee055f7020
haym23 <haym23@yahoo.com>
2017-07-15 21:59:13 -0400

    Add files via upload

---
+++ FABiddingOwner.py
@@ -0,0 +1,112 @@
+import smtplib
+import mimetypes
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+from email.message import Message
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from Tkinter import *
+
+fields = 'Owner Name', 'Player Name', 'Salary', '# of Years'
+TEXT_FILE = 'faBid.txt'
+USERNAME = ''
+PASSWORD = ''
+
+def send_mail(send_from, send_to, fileName, u, p):
+    emailfrom = send_from
+    emailto = send_to
+    fileToSend = fileName
+    username = u
+    password = p
+
+    msg = MIMEMultipart()
+    msg["From"] = emailfrom
+    msg["To"] = emailto
+    msg["Subject"] = "help I cannot send an attachment to save my life"
+    msg.preamble = "help I cannot send an attachment to save my life"
+    ctype, encoding = mimetypes.guess_type(fileToSend)
+    if ctype is None or encoding is not None:
+        ctype = "application/octet-stream"
+    maintype, subtype = ctype.split("/", 1)
+
+    if maintype == "text":
+        fp = open(fileToSend)
+        # Note: we should handle calculating the charset
+        attachment = MIMEText(fp.read(), _subtype=subtype)
+        fp.close()
+
+    elif maintype == "image":
+        fp = open(fileToSend, "rb")
+        attachment = MIMEImage(fp.read(), _subtype=subtype)
+        fp.close()
+
+    elif maintype == "audio":
+        fp = open(fileToSend, "rb")
+        attachment = MIMEAudio(fp.read(), _subtype=subtype)
+        fp.close()
+
+    else:
+        fp = open(fileToSend, "rb")
+        attachment = MIMEBase(maintype, subtype)
+        attachment.set_payload(fp.read())
+        fp.close()
+        encoders.encode_base64(attachment)
+
+    attachment.add_header("Content-Disposition", "attachment", filename=fileToSend)
+    msg.attach(attachment)
+
+    server = smtplib.SMTP("smtp.gmail.com:587")
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(emailfrom, emailto, msg.as_string())
+    server.quit()
+        
+def condense(bidFields):
+    code = ''
+    for field in bidFields:
+        code = code + str(field) + '|'
+    return code
+
+def encrypt(orig):
+    
+    return code
+
+def textFile(text):
+    f = open(TEXT_FILE, 'w')
+    f.write(text)
+    f.close()
+
+def fetch(entries):
+    orig = ''
+    for entry in entries:
+        field = entry[0]
+        text  = entry[1].get()
+        newStr += condense(text)
+    textFile(encrypt(orig))
+    
+
+def makeform(root, fields):
+   entries = []
+   for field in fields:
+      row = Frame(root)
+      lab = Label(row, width=15, text=field, anchor='w')
+      ent = Entry(row)
+      row.pack(side=TOP, fill=X, padx=5, pady=5)
+      lab.pack(side=LEFT)
+      ent.pack(side=RIGHT, expand=YES, fill=X)
+      entries.append((field, ent))
+   return entries
+
+if __name__ == '__main__':
+   root = Tk()
+   ents = makeform(root, fields)
+   root.bind('<Return>', (lambda event, e=ents: fetch(e)))   
+   b1 = Button(root, text='Submit',
+          command=(lambda e=ents: fetch(e)))
+   b1.pack(side=LEFT, padx=5, pady=5)
+   b2 = Button(root, text='Quit', command=root.quit)
+   b2.pack(side=LEFT, padx=5, pady=5)
+   root.mainloop()
+
